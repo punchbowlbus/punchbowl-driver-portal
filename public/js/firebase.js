21cf43888884d057e60cf8a91aac123684641fd4
Nalin Rajapaksha <nalin@punchbowlbus.com.au>
2026-06-02 13:42:15 +1000

    Start Firebase Cloud Messaging setup

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -2,8 +2,10 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebas
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 import { FIREBASE_CONFIG } from "./config.js";
+import { getMessaging } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-messaging.js";
 
 export const app = initializeApp(FIREBASE_CONFIG);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
+export const messaging = getMessaging(app);
