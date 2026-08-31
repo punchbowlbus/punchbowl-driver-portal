f5415e170fbd48048603e22e752f1006702785ea
punchbowlbus <nalin@punchbowlbus.com.au>
2026-08-31 12:06:34 +1000

    Keep Pre RMS Check as only RMS job option

--- public/js/workshop_pre_rms_option.js
+++ public/js/workshop_pre_rms_option.js
@@ -1,10 +1,17 @@
-// Adds the workshop's manual Pre RMS Check as a selectable Workshop job type.
+// Keep the workshop's manual Pre RMS Check as the only RMS-related Workshop job type.
 const jobType = document.getElementById("jobType");
-if (jobType && ![...jobType.options].some((o) => o.value === "Pre RMS Check")) {
-  const option = document.createElement("option");
-  option.value = "Pre RMS Check";
-  option.textContent = "Pre RMS Check";
-  const other = [...jobType.options].find((o) => o.value === "Other");
-  if (other) jobType.insertBefore(option, other);
-  else jobType.appendChild(option);
+
+if (jobType) {
+  [...jobType.options]
+    .filter((o) => o.value === "Safety Inspection")
+    .forEach((o) => o.remove());
+
+  if (![...jobType.options].some((o) => o.value === "Pre RMS Check")) {
+    const option = document.createElement("option");
+    option.value = "Pre RMS Check";
+    option.textContent = "Pre RMS Check";
+    const other = [...jobType.options].find((o) => o.value === "Other");
+    if (other) jobType.insertBefore(option, other);
+    else jobType.appendChild(option);
+  }
 }
